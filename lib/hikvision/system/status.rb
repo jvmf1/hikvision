fd363b03701ff1119e9c451deeaf334e61840f9a
jvmf1 <jvx8ss@gmail.com>
2022-12-06 12:03:42 -0300

    Added cpu_{utilization.description,load_status}

--- lib/hikvision/system/status.rb
+++ lib/hikvision/system/status.rb
@@ -1,7 +1,29 @@
 module Hikvision
   class System
+    attr_reader :sxml
+
     def uptime(options = {cache: false})
       @isapi.get_xml('/ISAPI/System/status', options).DeviceStatus.deviceUpTime.inner_html.to_i
+    end
+
+    def cpu_utilization
+      require_sxml
+      @sxml.CPUList.xpath('//CPU').map { |c| c.cpuUtilization.inner_html.to_i }
+    end
+
+    def cpu_description
+      require_sxml
+      @sxml.CPUList.xpath('//CPU').map { |c| c.cpuDescription.inner_html }
+    end
+
+    def load_status(options = {})
+      @sxml = @isapi.get_xml('/ISAPI/System/status', options).DeviceStatus
+    end
+
+    private
+
+    def require_sxml
+      raise "load_status is required" unless @sxml
     end
   end
 end
