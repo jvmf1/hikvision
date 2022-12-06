ff756fe8d3c8d135deaf972af58b023d05293917
jvmf1 <jvx8ss@gmail.com>
2022-12-06 09:10:50 -0300

    Added system.name

--- lib/hikvision/system/system.rb
+++ lib/hikvision/system/system.rb
@@ -1,11 +1,28 @@
 module Hikvision
   class System
+    attr_reader :dxml
+
     def initialize(isapi)
       @isapi = isapi
     end
 
     def reboot
       @isapi.put('/ISAPI/System/reboot')
+    end
+
+    def name
+      require_dxml
+      @dxml.deviceName.inner_html
+    end
+
+    def load_device_info
+      @dxml = @isapi.get_xml('/ISAPI/System/deviceInfo').DeviceInfo
+    end
+
+    private
+
+    def require_dxml
+      raise "load_device_info is required" unless @dxml
     end
   end
 end
