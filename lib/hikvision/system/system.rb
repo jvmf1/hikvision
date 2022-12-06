e22c25540db5ba97024c6ba32003125debf83bb0
jvmf1 <jvx8ss@gmail.com>
2022-12-06 09:13:54 -0300

    Added id, description, location

--- lib/hikvision/system/system.rb
+++ lib/hikvision/system/system.rb
@@ -15,6 +15,21 @@ module Hikvision
       @dxml.deviceName.inner_html
     end
 
+    def id
+      require_dxml
+      @dxml.deviceID.inner_html
+    end
+
+    def description
+      require_dxml
+      @dxml.deviceDescription.inner_html
+    end
+
+    def location
+      require_dxml
+      @dxml.deviceLocation.inner_html
+    end
+
     def load_device_info
       @dxml = @isapi.get_xml('/ISAPI/System/deviceInfo').DeviceInfo
     end
