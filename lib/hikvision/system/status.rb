f27762522e61e6f8b8a5be3746b543891403f059
jvmf1 <jvx8ss@gmail.com>
2022-12-12 12:04:29 -0300

    removed require_sxml

--- lib/hikvision/system/status.rb
+++ lib/hikvision/system/status.rb
@@ -7,38 +7,27 @@ module Hikvision
     end
 
     def memory_usage
-      require_sxml
       @sxml.xpath('MemoryList/Memory/memoryUsage').map { |m| m.inner_html.to_i }
     end
 
     def memory_available
-      require_sxml
       @sxml.xpath('MemoryList/Memory/memoryAvailable').map { |m| m.inner_html.to_i }
     end
 
     def memory_description
-      require_sxml
       @sxml.xpath('MemoryList/Memory/memoryDescription').map { |m| m.inner_html }
     end
 
     def cpu_utilization
-      require_sxml
       @sxml.xpath('CPUList/CPU/cpuUtilization').map { |c| c.inner_html.to_i }
     end
 
     def cpu_description
-      require_sxml
       @sxml.xpath('CPUList/CPU/cpuDescription').map { |c| c.inner_html }
     end
 
     def load_status(options = {})
       @sxml = @isapi.get_xml('/ISAPI/System/status', options).at_xpath('DeviceStatus')
-    end
-
-    private
-
-    def require_sxml
-      raise "load_status is required" unless @sxml
     end
   end
 end
