b0f5b9688129a2d9c97aafaa4786ab13bbcd471f
jvmf1 <jvx8ss@gmail.com>
2022-12-12 12:06:37 -0300

    Added base in system/time

--- lib/hikvision/system/time.rb
+++ lib/hikvision/system/time.rb
@@ -7,24 +7,11 @@ module Hikvision
       DateTime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
     end
 
-    def time_zone(options = {})
-      require_txml
-      @txml.at_xpath('timeZone').inner_html
-    end
-
-    def time_mode(options = {})
-      require_txml
-      @txml.at_xpath('timeMode').inner_html
-    end
+    add_getter(:time_zone, :@txml, 'timeZone', :to_s)
+    add_getter(:time_mode, :@txml, 'timeMode', :to_s)
 
     def load_time(options = {})
       @txml = @isapi.get_xml('/ISAPI/System/time', options).at_xpath('Time')
-    end
-
-    private
-
-    def require_txml
-      raise 'load_time is required' unless @txml
     end
   end
 end
