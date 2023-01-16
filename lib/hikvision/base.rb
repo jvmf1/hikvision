3bc3e498f3d735f9822f84a6b047cfe7c8369a1a
jvmf1 <jvx8ss@gmail.com>
2023-01-16 12:04:11 -0300

    Added nil return if at_xpath fails

--- lib/hikvision/base.rb
+++ lib/hikvision/base.rb
@@ -2,31 +2,51 @@ module Hikvision
   class Base
     def self.add_getter(method, xml, path, transform)
       define_method method do
-        instance_variable_get(xml).at_xpath(path).inner_html.send(transform)
+        begin
+          instance_variable_get(xml).at_xpath(path).inner_html.send(transform)
+        rescue
+          nil
+        end
       end
     end
 
     def self.add_list_getter(method, xml, path, transform)
       define_method method do
-        instance_variable_get(xml).xpath(path).map { |v| v.inner_html.send(transform) }
+        begin
+          instance_variable_get(xml).xpath(path).map { |v| v.inner_html.send(transform) }
+        rescue
+          nil
+        end
       end
     end
 
     def self.add_opt_getter(method, xml, path, transform)
       define_method method do
-        instance_variable_get(xml).at_xpath(path)[:opt].split(',').map { |v| v.send(transform) }
+        begin
+          instance_variable_get(xml).at_xpath(path)[:opt].split(',').map { |v| v.send(transform) }
+        rescue
+          nil
+        end
       end
     end
 
     def self.add_opt_range_getter(method, xml, path)
       define_method method do
-        instance_variable_get(xml).at_xpath(path)[:min].to_i..@cxml.at_xpath(path)[:max].to_i
+        begin
+          instance_variable_get(xml).at_xpath(path)[:min].to_i..@cxml.at_xpath(path)[:max].to_i
+        rescue
+          nil
+        end
       end
     end
 
     def self.add_bool_getter(method, xml, path)
       define_method method do
-        instance_variable_get(xml).at_xpath(path).inner_html == 'true'
+        begin
+          instance_variable_get(xml).at_xpath(path).inner_html == 'true'
+        rescue
+          nil
+        end
       end
     end
 
