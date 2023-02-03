11a2df55af5c0f99eada8de45e7e8f383492998a
jvmf1 <jvx8ss@gmail.com>
2023-02-03 09:11:53 -0300

    Added cache: false to memory_{usage,available} cpu_utilization

--- lib/hikvision/system/status.rb
+++ lib/hikvision/system/status.rb
@@ -4,10 +4,10 @@ module Hikvision
 
     add_getter(:uptime, :status, 'deviceUpTime', { cache: false }) { |v| v.to_i }
 
-    add_list_getter(:memory_usage, :status, 'MemoryList/Memory/memoryUsage') { |v| v.to_i }
-    add_list_getter(:memory_available, :status, 'MemoryList/Memory/memoryAvailable') { |v| v.to_i }
+    add_list_getter(:memory_usage, :status, 'MemoryList/Memory/memoryUsage', { cache: false }) { |v| v.to_i }
+    add_list_getter(:memory_available, :status, 'MemoryList/Memory/memoryAvailable', { cache: false }) { |v| v.to_i }
     add_list_getter(:memory_description, :status, 'MemoryList/Memory/memoryDescription')
-    add_list_getter(:cpu_utilization, :status, 'CPUList/CPU/cpuUtilization') { |v| v.to_i }
+    add_list_getter(:cpu_utilization, :status, 'CPUList/CPU/cpuUtilization', { cache: false }) { |v| v.to_i }
     add_list_getter(:cpu_description, :status, 'CPUList/CPU/cpuDescription')
   end
 end
