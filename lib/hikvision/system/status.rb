d86a44077f9f52c763d4aa4786d361715bd78366
jvmf1 <jvx8ss@gmail.com>
2023-02-03 09:06:34 -0300

    Moved uptime to add_getter

--- lib/hikvision/system/status.rb
+++ lib/hikvision/system/status.rb
@@ -1,10 +1,8 @@
 module Hikvision
   class System
-    def uptime(options = { cache: false })
-      @isapi.get_xml('/ISAPI/System/status', options).at_xpath('DeviceStatus/deviceUpTime').inner_html.to_i
-    end
-
     add_xml(:status, '/ISAPI/System/status', 'DeviceStatus')
+
+    add_getter(:uptime, :status, 'deviceUpTime', { cache: false }) { |v| v.to_i }
 
     add_list_getter(:memory_usage, :status, 'MemoryList/Memory/memoryUsage') { |v| v.to_i }
     add_list_getter(:memory_available, :status, 'MemoryList/Memory/memoryAvailable') { |v| v.to_i }
