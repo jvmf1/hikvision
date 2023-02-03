4dddad930cf82f03357dd22f6a2b51f14aa4fdf5
jvmf1 <jvx8ss@gmail.com>
2023-02-03 15:46:27 -0300

    Added specific placeholder for ISAPI/System/time (ISAPI.system.time.*)

--- lib/hikvision/system.rb
+++ lib/hikvision/system.rb
@@ -1,10 +1,11 @@
 module Hikvision
   class System < Hikvision::Base
-    attr_reader :network
+    attr_reader :network, :time
 
     def initialize(isapi)
       @isapi = isapi
       @network = Network.new(isapi)
+      @time = Time.new(isapi)
     end
   end
 end
--- lib/hikvision/system/time.rb
+++ lib/hikvision/system/time.rb
@@ -1,9 +1,21 @@
 module Hikvision
   class System
-    add_xml(:time, '/ISAPI/System/time', 'Time')
+    class Time < Hikvision::Base
+      class << self
+        def url
+          '/ISAPI/System/time'
+        end
+      end
 
-    add_getter(:time, :time, 'localTime', cache: false) { |v| DateTime.strptime(v, '%Y-%m-%dT%H:%M:%S%z') }
-    add_getter(:time_zone, :time, 'timeZone')
-    add_getter(:time_mode, :time, 'timeMode')
+      def initialize(isapi)
+        @isapi = isapi
+      end
+
+      add_xml(:base, url, 'Time')
+
+      add_getter(:mode, :base, 'timeMode')
+      add_getter(:datetime, :base, 'localTime', cache: false) { |v| DateTime.strptime(v, '%Y-%m-%dT%H:%M:%S%z') }
+      add_getter(:zone, :base, 'timeZone')
+    end
   end
 end
