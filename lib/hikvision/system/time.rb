c3d8d85201ef85376dfe5d88194fa480d9520b97
jvmf1 <jvx8ss@gmail.com>
2023-02-03 16:06:29 -0300

    Added setters {datetime,zone,mode}=

--- lib/hikvision/system/time.rb
+++ lib/hikvision/system/time.rb
@@ -14,8 +14,19 @@ module Hikvision
       add_xml(:base, url, 'Time')
 
       add_getter(:mode, :base, 'timeMode')
-      add_getter(:datetime, :base, 'localTime', cache: false) { |v| DateTime.strptime(v, '%Y-%m-%dT%H:%M:%S%z') }
+      add_setter(:mode=, :base, 'timeMode', Symbol, String)
+
+      add_getter(:datetime, :base, 'localTime', cache: false) { |v| DateTime.strptime(v, '%Y-%m-%dT%H:%M:%S%Z') }
+      add_setter(:datetime=, :base, 'localTime', DateTime) { |v| v.strftime('%Y-%m-%dT%H:%M:%S%Z') }
+
       add_getter(:zone, :base, 'timeZone')
+      add_setter(:zone=, :base, 'timeZone', String)
+
+      def update(options = {})
+        options[:body] = @base_xml.xpath('/').to_s
+
+        @isapi.put(self.class.url, options)
+      end
     end
   end
 end
