6e84277af00ce73ca0cc95280ccc7ac60b5de2ff
Cody Cutrer <cody@cutrer.us>
2023-02-21 08:36:43 -0700

    use pretzel colon operator to simplify many transformations

--- lib/hikvision/streaming/channel.rb
+++ lib/hikvision/streaming/channel.rb
@@ -8,22 +8,22 @@ module Hikvision
 
       add_xml(:base, -> { url }, 'StreamingChannel')
 
-      add_getter(:id, :base, 'id') { |v| v.to_i }
+      add_getter(:id, :base, 'id', &:to_i)
       add_getter(:name, :base, 'channelName')
-      add_getter(:max_packet_size, :base, 'Transport/maxPacketSize') { |v| v.to_i }
+      add_getter(:max_packet_size, :base, 'Transport/maxPacketSize', &:to_i)
       add_getter(:auth_type, :base, 'Transport/Security/certificateType')
       add_getter(:video_framerate, :base, 'Video/maxFrameRate') { |v| v.to_f / 100 }
-      add_getter(:video_width, :base, 'Video/videoResolutionWidth') { |v| v.to_i }
-      add_getter(:video_height, :base, 'Video/videoResolutionHeight') { |v| v.to_i }
-      add_getter(:video_cbitrate, :base, 'Video/constantBitRate') { |v| v.to_i }
-      add_getter(:video_vbitrate_upper_cap, :base, 'Video/vbrUpperCap') { |v| v.to_i }
+      add_getter(:video_width, :base, 'Video/videoResolutionWidth', &:to_i)
+      add_getter(:video_height, :base, 'Video/videoResolutionHeight', &:to_i)
+      add_getter(:video_cbitrate, :base, 'Video/constantBitRate', &:to_i)
+      add_getter(:video_vbitrate_upper_cap, :base, 'Video/vbrUpperCap', &:to_i)
       add_getter(:video_keyframe_interval, :base, 'Video/keyFrameInterval') { |v| v.to_i / 1000 }
       add_getter(:video_codec, :base, 'Video/videoCodecType')
       add_getter(:video_bitrate_type, :base, 'Video/videoQualityControlType')
       add_getter(:video_scan_type, :base, 'Video/videoScanType')
       add_getter(:snapshot_image_type, :base, 'Video/snapShotImageType')
       add_getter(:audio_codec, :base, 'Audio/audioCompressionType')
-      add_getter(:video_smoothing, :base, 'Video/smoothing') { |v| v.to_i }
+      add_getter(:video_smoothing, :base, 'Video/smoothing', &:to_i)
 
       add_bool_getter(:enabled?, :base, 'enabled')
       add_bool_getter(:svc_enabled?, :base, 'Video/SVC/enabled')
