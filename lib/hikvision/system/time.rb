91e14b4a46f0ffe39a310807c389c0f8a056fcb7
jvmf1 <jvx8ss@gmail.com>
2023-02-23 09:01:54 -0300

    Fix class name conflict

--- lib/hikvision/system/time.rb
+++ lib/hikvision/system/time.rb
@@ -18,8 +18,8 @@ module Hikvision
       add_getter(:mode, :base, 'timeMode', &:to_sym)
       add_setter(:mode=, :base, 'timeMode', Symbol, String)
 
-      add_getter(:now, :base, 'localTime', cache: false) { |v| Time.strptime(v, '%Y-%m-%dT%H:%M:%S%Z') }
-      add_setter(:now=, :base, 'localTime', Time) { |v| v.strftime('%Y-%m-%dT%H:%M:%S%Z') }
+      add_getter(:now, :base, 'localTime', cache: false) { |v| Object::Time.strptime(v, '%Y-%m-%dT%H:%M:%S%Z') }
+      add_setter(:now=, :base, 'localTime', Object::Time) { |v| v.strftime('%Y-%m-%dT%H:%M:%S%Z') }
 
       add_getter(:zone, :base, 'timeZone')
       add_setter(:zone=, :base, 'timeZone', String)
